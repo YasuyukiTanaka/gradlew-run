7fd5348a34685f917adfed1418ac8f9e7603c6bf
vagrant <vagrant@vagrant.vm>
2020-05-18 14:56:02 +0900

    2020/05/18 14:55

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
-const path = require('path');
 const process = require('process');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec)
-
-
-async function lsExample() {
-  try {
-    const { stdout, stderr } = await exec('ls');
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-  } catch (e) {
-    console.error(e); // should contain code (exit code) and signal (that caused the termination).
-  }
-}
-
-async function pwdExample() {
-  try {
-    const { stdout, stderr } = await exec('pwd');
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-  } catch (e) {
-    console.error(e); // should contain code (exit code) and signal (that caused the termination).
-  }
-}
 
 async function gradlewExecute(task) {
   try {
@@ -46,27 +24,10 @@ try {
   const workingDirectory = core.getInput('working-directory');
   console.log(`working-directory param is ${workingDirectory}!`);
  
-  //var dirname = path.dirname(workingDirectory);
-  //console.log(`dirname is ${dirname}`);
   fs.statSync(workingDirectory);
-  // fs.statSync(workingDirectory);
-  console.log("process.cwd() = " + process.cwd());
   process.chdir(workingDirectory);
-  // console.log(dirname);
-  console.log("process.cwd() = " + process.cwd());
-  lsExample();
   
-  pwdExample();
   gradlewExecute(task);
-  //const { stdout, stderr } = (async () => { await exec(`./gradlew ${task}`); })()
-  // const { stdout, stderr } = await exec(`./gradlew ${shadowjar}`);  
-  //console.log(`stdout: ${stdout}`);
-  //console.log(`stderr: ${stderr}`);
-  //const time = (new Date()).toTimeString();
-  //core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
